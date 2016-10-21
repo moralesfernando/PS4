c264a0f21bd056af3683cbc6683f880f6b8aaad2
moral <moral@DESKTOP-TPSHLG8>
2016-10-21 04:43:31 -0400

    n

--- AddressApp/src/ch/makery/address/model/Person.java
+++ AddressApp/src/ch/makery/address/model/Person.java
@@ -2,9 +2,6 @@ package ch.makery.address.model;
 
 import java.time.LocalDate;
 
-import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
-
-import ch.makery.address.util.LocalDateAdapter;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleIntegerProperty;
@@ -110,7 +107,6 @@ public class Person {
         return city;
     }
 
-    @XmlJavaTypeAdapter(LocalDateAdapter.class)
     public LocalDate getBirthday() {
         return birthday.get();
     }
